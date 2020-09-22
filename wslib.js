f83088a47a5692bbf4edb62fbe0827f7d24fd86f
Juan Diego Trujillo Montoya <jd.trujillom@uniandes.edu.co>
2020-09-22 15:28:28 -0500

    Los mensajes ahora son persistentes

--- wslib.js
+++ wslib.js
@@ -1,18 +1,28 @@
 const WebSocket = require("ws");
+const fs = require("fs");
+
+const URL_MESSAGES_JSON = "./storage/messages.json";
 
 const clients = [];
 const messages = [];
 
 const wsConnection = (server) => {
     const wss = new WebSocket.Server({server});
-
+    const messages = JSON.parse(fs.readFileSync(URL_MESSAGES_JSON));
+    console.log("Messages load!");
     wss.on("connection", (ws) => {
         clients.push(ws);
         sendMessages();
 
         ws.on("message", (message) => {
             messages.push(message);
+
+            fs.writeFile(URL_MESSAGES_JSON, JSON.stringify(messages), () => {
+                console.log("messages saved");
+            });
+
             sendMessages();
+
         });
     });
 
